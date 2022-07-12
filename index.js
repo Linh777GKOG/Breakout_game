53dacda19d383d68348e9674ba3c267e39f330cf
Linh Vu <linh777gkog@gmail.com>
2022-07-13 01:01:10 +0700

    draw user

--- index.js
+++ index.js
@@ -91,3 +91,9 @@ function moveUser(e) {
   }
 }
 document.addEventListener('keydown', moveUser);
+
+//draw User
+function drawUser() {
+  user.style.left = currentPosition[0] + 'px';
+  user.style.bottom = currentPosition[1] + 'px';
+}
