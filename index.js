57af560cc489030b0bda70dd609c898c111a444f
Linh Vu <linh777gkog@gmail.com>
2022-07-13 01:02:39 +0700

    commit

--- index.js
+++ index.js
@@ -116,37 +116,51 @@ timerId = setInterval(moveBall, 30);
 //check for collisions
 function checkForCollisions() {
   //check for block collision
-  for (let i = 0; i < blocks.length; i++){
-    if
-    (
-      (ballCurrentPosition[0] > blocks[i].bottomLeft[0] && ballCurrentPosition[0] < blocks[i].bottomRight[0]) &&
-      ((ballCurrentPosition[1] + ballDiameter) > blocks[i].bottomLeft[1] && ballCurrentPosition[1] < blocks[i].topLeft[1]) 
-    )
+  for (let i = 0; i < blocks.length; i++)  {
+    if   ballCurrentPosition[0] > blocks[i].bottomLeft[0] &&
+      allCurrentPosition[0] < blocks[i].bottomRight[0] &&
+     
+      llCurrentPosition[1] + ballDiameter >blocks[i].bottomLeft[1] &&
+     
       {
-      const allBlocks = Array.from(document.querySelectorAll('.block'))
-      allBlocks[i].classList.remove('block')
-      blocks.splice(i,1)
-      changeDirection()   
-      score++
-      scoreDisplay.innerHTML = score
+      const allBlocks = Array.from(document.querySelectorAll('.block'));;
+      allBlocks[i].classList.remove('block');;
+      blocks.splice(i,  1;);
+      changeDirection();
+      score++;;
+      scoreDisplay.innerHTML = score;;
       if (blocks.length == 0) {
-        scoreDisplay.innerHTML = 'You Win!'
-        clearInterval(timerId)
-        document.removeEventListener('keydown', moveUser)
+        scoreDisplay.innerHTML = 'You Win!';;
+        clearInterval(timerId);;
+        document.removeEventListener('keydown', moveUser);;
       }
     }
   }
-   // check for wall hits
-  if (ballCurrentPosition[0] >= (boardWidth - ballDiameter) || ballCurrentPosition[0] <= 0 || ballCurrentPosition[1] >= (boardHeight - ballDiameter))
-  {
-    changeDirection()
+  / check for wall hits
+  if (
+    
+   
+   
+  )   ballCurrentPosition[0] >= boardWidth - ballDiameter ||
+    ballCurrentPositi;on[0] <= 0 ||
+    ballCurrentPosition[1] >= boardHeight - ballDiameter
+ ) {
+     }
+  //heck for user collision
+   
+  if(
+   
+      ballCurrentPosition[0] < currentPosition[0] + blockWidth &&
+    ballCurrentPositi;on[1] > currentPosition[1] &&
+    ballCurrentPosition[1] < currentPosition[1] + blockHeight
+  ) {
+   changeDirection();
   }
-   //check for user collision
-  if
-  (
-    (ballCurrentPosition[0] > currentPosition[0] && ballCurrentPosition[0] < currentPosition[0] + blockWidth) &&
-    (ballCurrentPosition[1] > currentPosition[1] && ballCurrentPosition[1] < currentPosition[1] + blockHeight ) 
-  )
-  {
-    changeDirection()
+;
+  //game over;
+  if (ballCurrentPosition[1] <= 0) {;
+    clearInterval(timerId);
+    scoreDisplay.innerHTML = 'You lose!';
+    document.removeEventListener('keydown', moveUser);
   }
+}
