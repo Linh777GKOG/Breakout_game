0aacf6b806d445c01b2dd79b94f599c6160371f2
Linh Vu <linh777gkog@gmail.com>
2022-07-13 00:59:49 +0700

    create block

--- index.js
+++ index.js
@@ -16,3 +16,13 @@ let ballCurrentPosition = ballStart;
 
 let timerId;
 let score = 0;
+
+//my block
+class Block {
+  constructor(xAxis, yAxis) {
+    this.bottomLeft = [xAxis, yAxis];
+    this.bottomRight = [xAxis + blockWidth, yAxis];
+    this.topRight = [xAxis + blockWidth, yAxis + blockHeight];
+    this.topLeft = [xAxis, yAxis + blockHeight];
+  }
+}
